d489eb700f33512e8fe2827440ce32d56f5685d1
miami-bear <lapinski215@gmail.com>
2024-12-22 22:49:04 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    stationCodes: {
+        'Białystok': 6,
+        'Warszawa Centralna': 242,
+    },
+    apiConfig:{
+        url: 'https://api-gateway.intercity.pl/server/public/endpoint',
+        urzadzenieNr: "956"
+    }
+}
